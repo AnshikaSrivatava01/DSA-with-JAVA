3c4a9979f27c39773393d3923724d19f186bb827
AnshikaSrivatava01 <anshika.io143@gmail.com>
2026-06-21 21:33:29 +0530

    Time: 3 ms (1.57%), Space: 48.2 MB (83.88%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l =0;
+        int r = nums.length-1;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < target){
+                l = l+1;
+            }else{
+                r = r-1;
+            }
+        }
+        return -1;
+    }
+}
